fa0b3262c895045b128dfb2f7e39faec4204049b
Alessandro Venturini <ales.ventus21@gmail.com>
2025-09-27 16:11:10 +0200

    init pygame image as black screen

--- src/main.py
+++ src/main.py
@@ -13,9 +13,9 @@ DT = 1 / FRAMERATE
 
 # Render object to keep and pass the PyGame surface
 @final
-class RenderObject(object):
+class RenderObject:
     def __init__(self, width: int, height: int):
-        init_image = np.random.randint(0,255,(height,width,3),dtype='uint8')
+        init_image = np.zeros((height, width, 3), dtype='uint8')
         self.surface = pygame.surfarray.make_surface(init_image.swapaxes(0,1))
 
 # Camera sensor callback, reshapes raw data from camera into 2D RGB and applies to PyGame surface
